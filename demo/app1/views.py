7ca9818504ca7ea5e9c0c94f4b83fb75a8ff9ef1
unknown <anasmuhd010@gmail.com>
2026-04-28 19:52:16 +0530

    all commits

---
+++ demo/app1/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def firstExample(req):
+    return HttpResponse("response")
+
+
+def secondReq(req):
+    return HttpResponse("<h1>responce</h1>")
+
+def homeView(req):
+    return render(req,"home.html")
+
+def aboutView(req):
+    return render(req,"about.html")
+
+def worksView(req):
+    username="Arun"
+    services=["web development","SEO","Digital Marketing","Graphics Designer"]
+
+    # works=[
+    #     {"id":1,"title":"Ecommerce app","client":"Amal","price":1200},
+    #     {"id":2,"title":"School Mng Ststem","client":"Vinu","price":1500},
+    #     {"id":3,"title":"Inventory Management","client":"Amal","price":2200},
+
+
+
+    # ]
+    works=[]
+    return render(req,"works.html",{"u_name":username,"sevice_list":services,"works":works})
+
+def addWorksView(req):
+    return render(req,"addworks.html")
